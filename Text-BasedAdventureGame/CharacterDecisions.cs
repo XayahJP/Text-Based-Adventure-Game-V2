5136f7c30762b27c1a39d0a78c9afc940a4e7de6
XayahJP <XayahJP@gmail.com>
2018-06-01 16:22:05 -0600

    A few small adjustments, primarily to the variables.

--- Text-BasedAdventureGame/CharacterDecisions.cs
+++ Text-BasedAdventureGame/CharacterDecisions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
 namespace Text_BasedAdventureGame
 {
     class DecisionsArray
@@ -97,3 +98,4 @@ namespace Text_BasedAdventureGame
         }
     }
 }
+*/
--- Text-BasedAdventureGame/CharacterStats.cs
+++ Text-BasedAdventureGame/CharacterStats.cs
@@ -11,15 +11,7 @@ namespace Text_BasedAdventureGame
         public int Strength { get; }
         public int Luck { get; }
         public int Health { get; }
-
-        //public DamageRange damage { get; }
-        // Can I set DamageRange to an array, or only an enum? If I can set to an array, how?
-
-        /*
-        public int[] DamageRange = {
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-        };
-        */
+        private Random _damageRange { get; }
 
         public Stats(int strength, int luck, int health)
         {
@@ -27,6 +19,14 @@ namespace Text_BasedAdventureGame
             Luck = luck;
             Health = health;
             var StartingCharStats = new Stats(1, 0, 10);
+        }
+
+        public Stats()
+        {
+            // Damage range for the character
+            // Make sure it works first, then later make damage equal to: _damageRange + Strength
+            _damageRange = new Random();
+            _damageRange.Next(0, 3);
         }
     }
 }
